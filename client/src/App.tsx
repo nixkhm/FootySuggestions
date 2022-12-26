dbf77d0a5feab50b1bad1e45acf37a2a5cd8b57f
Nicholas Masters <nicholas.masters20@stjohns.edu>
2022-12-25 22:07:11 -0500

    Teams can be added by users and populates the UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,61 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
+//Team object which holds the PK and Name of the Team
+type TTeam = {
+  name: string;
+  _id: string;
+}
+
 function App() {
+  //user input for adding Teams to the DB
   const [name, setName] = useState('');
+  //array for holding the existing teams in the DB
+  const [teams, setTeams] = useState<TTeam[]>([]);
+
+  //function to create Teams to be added to the DB by users
+  async function handleCreateTeam(e: React.FormEvent){
+    e.preventDefault();
+
+    await fetch('http://localhost:8000/teams', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+      }),
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    });
+    setName("")
+  }
+
+  //function to fetch the teams that are in the database when the page is loaded
+  useEffect(() => {
+    async function fetchTeams(){
+      const response  = await fetch('http://localhost:8000/teams');
+      const newTeams = await response.json(); 
+      setTeams(newTeams);
+    }
+    fetchTeams();
+  }, []);
 
   return (
     <div className="App">
-    <form>
+      <ul className="teams">
+        {teams.map((team) => (
+            <li key = {team._id}> {team.name} </li>
+        ))
+        }
+    </ul>
+    <form onSubmit= {handleCreateTeam}>
       <label htmlFor="team-name">Team Name</label>
       <input id="team-name"
         value = {name}
         onChange = {(e: React.ChangeEvent<HTMLInputElement>) =>
-          //save what the user entered
           setName(e.target.value)
         }
       />
+      <button>Create Team</button>
     </form>
     </div>
   )
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,27 @@
-import {config} from "dotenv";
-config();
-
-import express, {Request, Response} from "express"
+import express, {Request, Response} from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import Team from './models/Team';
+
+import {config} from "dotenv";
+config();
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(cors({
+    origin: "*",
+}));
 
+//API GET request for the Team model in the DB
+app.get("/teams", async (req: Request, res: Response) => {
+    const teams = await Team.find();
+    res.json(teams);
+});
+
+//API POST request for users to add teams to the DB
 app.post("/teams", async (req: Request, res: Response) => {
     const newTeam = new Team({
         name: req.body.name,
@@ -19,7 +30,7 @@ app.post("/teams", async (req: Request, res: Response) => {
     res.json(createdTeam);
 });
 
-
+//Connecting the DB to the Server
 mongoose.connect(process.env.MONGO_URL ?? "").then(() => {
     console.log(`listening on port ${PORT}`);
     app.listen(PORT);
